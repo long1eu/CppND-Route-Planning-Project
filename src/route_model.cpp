b14026e85acbc38be1aeb34d4f42d0511ca9d398
Razvan Cristian Lung <lung.razvan@gmail.com>
2019-07-01 15:31:46 +0300

    completed until lesson 12 inclusive

--- src/route_model.cpp
+++ src/route_model.cpp
@@ -1,6 +1,22 @@
-#include "route_model.h"
 #include <iostream>
+#include <cmath>
+#include "route_model.h"
+#include "model.h"
+
+using std::sqrt;
+using std::pow;
 
 RouteModel::RouteModel(const std::vector<std::byte> &xml) : Model(xml) {
 
+    int size = Nodes().size();
+    for (int i = 0; i < size; i++) {
+        Model::Node node = Nodes()[i];
+
+        m_Nodes.emplace_back(i, this, node);
+    }
+
+}
+
+float RouteModel::Node::Distance(const RouteModel::Node &node) const {
+    return sqrt(pow(x - node.x, 2) + pow(y - node.y, 2));
 }
--- src/route_model.h
+++ src/route_model.h
@@ -8,25 +8,40 @@
 
 class RouteModel : public Model {
 
-  public:
+public:
     class Node : public Model::Node {
-      public:
+    public:
         // Add public Node variables and methods here.
-        
-        Node(){}
-        Node(int idx, RouteModel * search_model, Model::Node node) : Model::Node(node), parent_model(search_model), index(idx) {}
-      
-      private:
+
+        Node() {}
+
+        Node(int idx, RouteModel *search_model, Model::Node node) : Model::Node(node),
+                                                                    parent_model(search_model),
+                                                                    index(idx) {}
+
+        Node *parent = nullptr;
+        float h_value = std::numeric_limits<float>::max();
+        float g_value = 0;
+        bool visited = false;
+        std::vector<Node *> neighbors;
+
+        float Distance(const Node &node) const;
+
+
+    private:
         // Add private Node variables and methods here.
         int index;
-        RouteModel * parent_model = nullptr;
+        RouteModel *parent_model = nullptr;
     };
-    
-    // Add public RouteModel variables and methods here.
-    RouteModel(const std::vector<std::byte> &xml);  
-    std::vector<Node> path; // This variable will eventually store the path that is found by the A* search.
 
-  private:
+    // Add public RouteModel variables and methods here.
+    explicit RouteModel(const std::vector<std::byte> &xml);
+
+    std::vector<Node> path; // This variable will eventually store the path that is found by the A* search.
+    std::vector<Node> &getSNodes() { return m_Nodes; }
+
+private:
     // Add private RouteModel variables and methods here.
+    std::vector<Node> m_Nodes;
 
 };
